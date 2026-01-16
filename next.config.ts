234ff8cf0df43b7b97688526de21de806f4648dd
themeede <74hm1dj@gmail.com>
2026-01-15 17:53:12 -0800

    undo

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    instrumentationHook: true,
-  },
 
   generateBuildId: () => packageJson.version,
 };
